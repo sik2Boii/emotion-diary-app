e33f62e0ce7022544d838b7aedfaebac338a12b5
Junsik <junsik213@naver.com>
2024-08-19 03:48:54 +0900

    React 동적 경로

--- frontend/src/pages/Diary.jsx
+++ frontend/src/pages/Diary.jsx
@@ -1,5 +1,10 @@
+import { useParams } from "react-router-dom";
+
 const Diary = () => {
-  return <div>Diary</div>;
+  // useParams를 사용하여 URL 파라미터에 접근
+  const params = useParams();
+
+  return <div>Diary: {params.id}</div>;
 };
 
 export default Diary;
