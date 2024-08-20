40331062f66c43032e998e66fd870eda8b3e9dcf
Junsik <junsik213@naver.com>
2024-08-21 01:40:41 +0900

    Edit 페이지 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
+import Button from "./components/Button";
+import Header from "./components/Header";
 import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
-import { getEmotionImage } from "./util/get-emotion-image";
-import Button from "./components/Button";
-import Header from "./components/Header";
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new": 새로운 일기를 작성하는 New 페이지
-// 3. "/diary": 특정 일기를 상세히 조회하는 Diary 페이지
-// 4. "*": 정의되지 않은 모든 경로에 대해 Notfound 페이지를 표시
+// 1. "/" : 모든 일기를 조회하는 페이지
+// 2. "/new": 새로운 일기를 작성하는 페이지
+// 3. "/diary": 특정 일기를 상세히 조회하는 페이지
+// 4. "/edit": 특정 일기를 수정하는 페이지
+// 5. "*": 정의되지 않은 모든 경로에 대해 Notfound 페이지를 표시
 
 function App() {
   const nav = useNavigate();
@@ -29,33 +30,11 @@ function App() {
         rightChild={<Button text={"Right"} />}
       />
 
-      <Button
-        text={"111"}
-        onClick={() => {
-          console.log("111");
-        }}
-      />
-
-      <Button
-        text={"222"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("222");
-        }}
-      />
-
-      <Button
-        text={"333"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("333");
-        }}
-      />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
