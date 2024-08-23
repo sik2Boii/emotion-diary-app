1bc4b437cd3c8472aa80c095e1d6a4f45d387a3e
Junsik <junsik213@naver.com>
2024-08-23 16:10:54 +0900

    New 페이지 UI 구현

---
+++ frontend/src/components/Editor.jsx
@@ -0,0 +1,62 @@
+import Button from "./Button";
+import "./Editor.css";
+import EmotionItem from "./EmotionItem";
+
+// 감정 리스트
+const emotionList = [
+  {
+    emotionId: 1,
+    emotionName: "최고",
+  },
+  {
+    emotionId: 2,
+    emotionName: "좋음",
+  },
+  {
+    emotionId: 3,
+    emotionName: "그냥저냥",
+  },
+  {
+    emotionId: 4,
+    emotionName: "나쁨",
+  },
+  {
+    emotionId: 5,
+    emotionName: "끔찍",
+  },
+];
+
+const Editor = () => {
+  const emotionId = 1;
+
+  return (
+    <div className="Editor">
+      <section className="date_section">
+        <h4>오늘의 날짜</h4>
+        <input type="date" />
+      </section>
+      <section className="emotion_section">
+        <h4>오늘의 감정</h4>
+        <div className="emotion_list_wrapper">
+          {emotionList.map((item) => (
+            <EmotionItem
+              key={item.emotionId}
+              {...item}
+              isSelected={item.emotionId === emotionId}
+            />
+          ))}
+        </div>
+      </section>
+      <section className="content_section">
+        <h4>오늘의 일기</h4>
+        <textarea placeholder="오늘은 어땠나요?" />
+      </section>
+      <section className="button_section">
+        <Button text={"취소하기"} />
+        <Button text={"작성완료"} type={"POSITIVE"} />
+      </section>
+    </div>
+  );
+};
+
+export default Editor;
